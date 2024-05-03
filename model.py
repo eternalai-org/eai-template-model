ece1218ec25d3ecbf62259fcd75330a5dde5bc0e
1026@newbitcoincity.com <1026@newbitcoincity.com>
2024-05-03 07:30:48 +0000

    refactor code

--- model.py
+++ model.py
@@ -6,8 +6,7 @@ class ImageGenerator:
     def __init__(self, config, device=None):
         self.device = device
         if self.device is None:
-            self.device = torch.device(
-                "cuda") if torch.cuda.is_available() else torch.device("cpu")
+            self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.config = config
         self.pipeline = self._load_pipeline()
         self.generator = torch.Generator(device=self.device)
@@ -15,42 +14,37 @@ class ImageGenerator:
     def _load_pipeline(self):
         ckpt_type = self.config["checkpoint_type"]
         if ckpt_type == "safetensors":
-            pipeline = diffusers.StableDiffusionXLPipeline.from_single_file(
-                self.config["model_ckpt"]).to(self.device)
+            pipeline = diffusers.StableDiffusionXLPipeline.from_single_file(self.config["model_ckpt"]).to(self.device)
         elif ckpt_type == "lora":
             base_model_ckpt = self.config["base_model_ckpt"]
             base_model = os.path.basename(base_model_ckpt).split("_")[0]
             if base_model == "sdxl":
-                pipeline = diffusers.StableDiffusionXLPipeline.from_single_file(
-                    base_model_ckpt).to(self.device)
+                pipeline = diffusers.StableDiffusionXLPipeline.from_single_file(base_model_ckpt).to(self.device)
             else:
-                pipeline = diffusers.StableDiffusionPipeline.from_pretrained(
-                    base_model_ckpt).to(self.device)
+                pipeline = diffusers.StableDiffusionPipeline.from_pretrained(base_model_ckpt).to(self.device)
             pipeline.load_lora_weights(self.config["model_ckpt"])
         elif ckpt_type == "diffusers":
-            pipeline = diffusers.StableDiffusionPipeline.from_pretrained(
-                self.config["model_ckpt"]).to(self.device)
+            pipeline = diffusers.StableDiffusionPipeline.from_pretrained(self.config["model_ckpt"]).to(self.device)
         else:
             raise ValueError(f"Unknown checkpoint type: {ckpt_type}")
         return pipeline
 
-    def __call__(self, prompt, seed):
+    def __call__(self, prompt, seed, h=None, w=None, steps=None, cfg=None):
+        h = h or self.config["height"]
+        w = w or self.config["width"]
+        steps = steps or self.config["steps"]
+        cfg = cfg or self.config["cfg"]
+
         self.generator.manual_seed(seed)
-        image_latents = torch.randn(
-            (1,
-             self.pipeline.unet.config.in_channels,
-             self.config["height"] // 8,
-             self.config["width"] // 8),
-            generator=self.generator,
-            device=self.device)
+        image_latents = torch.randn((1, self.pipeline.unet.config.in_channels, h // 8, w // 8), generator=self.generator, device=self.device)
         pil_images = self.pipeline(
             prompt,
             latents=image_latents,
-            height=self.config["height"],
-            width=self.config["width"],
-            guidance_scale=self.config["cfg"],
+            height=h,
+            width=w,
+            guidance_scale=cfg,
             num_images_per_prompt=1,
-            num_inference_steps=self.config["steps"],
+            num_inference_steps=steps,
         ).images
         checked_image = pil_images[0]
         torch.cuda.empty_cache()
--- predict.py
+++ predict.py
@@ -20,10 +20,23 @@ class Predictor(BasePredictor):
             description="Prompt to generate an image from"),
         seed: int = Input(
             description="Seed for random number generator",
-            default = 0),
+            default=0),
+        h: int = Input(
+            description="Height of the image",
+            default=None),
+        w: int = Input(
+            description="Width of the image",
+            default=None),
+        steps: int = Input(
+            description="Number of inference steps",
+            default=None),
+        cfg: float = Input(
+            description="Guidance scale",
+            default=None),
         output_path: str = Input(
             description="Path to save the generated image or to check the image",
-            default = "output.jpg")) -> str:
-        checked_image = self.model(prompt, seed)
+            default="output.jpg")
+    ) -> str:
+        checked_image = self.model(prompt, seed, h, w, steps, cfg)
         checked_image.save(output_path, optimize=True, quality=40)
         return output_path
